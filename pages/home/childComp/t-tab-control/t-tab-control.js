5e9f5a7c325f5e0633b9ff70a872838616b2802a
2931164745@qq.com <18340873281aa>
2021-01-07 22:45:12 +0800

    筛选完成

--- pages/home/childComp/t-tab-control/t-tab-control.js
+++ pages/home/childComp/t-tab-control/t-tab-control.js
@@ -11,18 +11,102 @@ Component({
    * 组件的初始数据
    */
   data: {
-    isShow: false
+    isShow: false,
+    weapon_type: {
+      title: "武器类型",
+      list: [{
+          text: "全部",
+          value: "全部"
+        }, 
+        {
+          text: "双手剑",
+          value: "双手剑"
+        },
+        {
+          text: "单手剑",
+          value: "单手剑"
+        },
+        {
+          text: "法器",
+          value: "法器"
+        },
+        {
+          text: "弓",
+          value: "弓"
+        },
+        {
+          text: "长柄武器",
+          value: "长柄武器"
+        }
+      ]
+    },
+    weapon_qulity: {
+      title: "武器品质",
+      list: [{
+          text: "全部",
+          value: 0
+        },
+        {
+          text: "5星",
+          value: 5
+        },
+        {
+          text: "4星",
+          value: 4
+        },
+        {
+          text: "3星",
+          value: 3
+        },
+        {
+          text: "2星",
+          value: 2
+        },
+        {
+          text: "1星",
+          value: 1
+        }
+      ]
+    },
+    select: {
+      typeValue: '全部',
+      typeIndex: 0,
+      qulityValue: '全部',
+      qulityIndex: 0
+    }
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    handShowBtnClick(){
-      console.log("--------------")
+    handShowBtnClick() {
+      // console.log("--------------")
       this.setData({
         isShow: !this.data.isShow
       })
+    },
+    handItemClick(options){
+      let index = options.currentTarget.dataset.index;
+      let type = options.currentTarget.dataset.type;
+
+      let select = this.data.select;
+      if(type == 'type'){
+        select.typeIndex = index
+        select.typeValue = this.data.weapon_type.list[index].value
+      }
+
+      if(type == 'qulity'){
+        select.qulityIndex = index
+        select.qulityValue = this.data.weapon_qulity.list[index].value
+      }
+
+      this.setData({
+        select: select
+      })
+
+      // console.log(select);
+      this.triggerEvent("selectChange", select);
     }
   }
 })
--- pages/home/home.js
+++ pages/home/home.js
@@ -7,7 +7,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    weapon_list: []
+    weapon_list: [],
+    show_weapon_data: []
   },
 
   /**
@@ -26,8 +27,35 @@ Page({
       url: '/weapon/list'
     }).then(res => {
       this.setData({
-        weapon_list: res.data
+        weapon_list: res.data,
+        show_weapon_data: res.data
       })
     })
+  },
+
+  handleSelectChange(options){
+    let typeValue = options.detail.typeValue;
+    let qulityValue = options.detail.qulityValue;
+    
+    let show_weapon_data = this.data.weapon_list;
+
+    //按照武器类型筛选数据
+    if(typeValue != '全部'){
+      show_weapon_data = show_weapon_data.filter(item => {
+        return item.weaponType.typeName == typeValue
+      });
+    }
+
+    //按照品质筛选数据
+    if(qulityValue != '全部'){
+      show_weapon_data = show_weapon_data.filter(item => {
+        return item.quality == qulityValue
+      });
+    }
+
+    this.setData({
+      show_weapon_data: show_weapon_data
+    });
+    
   }
 })
